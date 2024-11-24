fd28a111ead90d710a38095ce24af1d9e3df4c13
arufiand <alfiandannyarmanta@gmail.com>
2024-11-24 16:37:40 +0700

    feat: Screen Animation
    Adding screen and widgets for cats
    
    Author: @arufiand

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:animation/src/widgets/cat.dart';
+
+class Home extends StatefulWidget{
+  const Home({super.key});
+
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home>{
+  @override
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title : const Text('Animation'),
+      ),
+      body: buildAnimation(),
+    );
+  }
+  Widget buildAnimation(){
+    return const Cat();
+  }
+}
